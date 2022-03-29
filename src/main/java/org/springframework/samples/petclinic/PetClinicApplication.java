8f162652b2e7b429fb4b386850c87b6e1533fdf7
JamesTD1949 <55186098+JamesTD1949@users.noreply.github.com>
2022-03-29 19:21:55 +0100

    format applied

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -1,6 +1,8 @@
 package org.springframework.samples.petclinic;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * PetClinic Spring Boot Application.
  *
@@ -8,5 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-public class PetClinicApplication{public static void main(String[] args) {SpringApplication.run(PetClinicApplication.class, args);}}
+public class PetClinicApplication {
 
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicApplication.class, args);
+	}
+
+}
